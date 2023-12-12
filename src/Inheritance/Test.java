8f4c7392cbedf3a96b43e02eb24d0b9608e5ff34
121321033014 <149389539+121321033014@users.noreply.github.com>
2023-12-12 14:53:00 +0530

    second commit

---
+++ src/Inheritance/Test.java
@@ -0,0 +1,12 @@
+package Inheritance;
+
+public class Test {
+    public static void main(String[] args) {
+        //creating objects in java
+        Dog d=new Dog();
+        d.name="Buddy ";
+        d.bark();
+        d.eat();
+
+    }
+}
